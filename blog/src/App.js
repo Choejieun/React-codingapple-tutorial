5c2f826e92e9b58ece3d3a02669a6e5d9a99ebd9
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-06-18 20:46:10 +0900

    배열추가 연산

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,16 +9,12 @@ function App() {
   let post = '강남 초밥 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 맛집 추천', '코드추천 강남']);
   let [따봉넘버, 따봉넘버변경] = useState([0,0,0]);
-  // a는 보관된 이름
-  // b는 변경을 도와줌
 
-  let [modal, setModal] = useState([false,false,false]);
-  //모달용, 변경어는 set~로 하는 게 기본
-  //형식은자유, 모달창 상태 표현만 가능 가능하게
-
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0); //모달용 변수
+  let [입력값, 입력값변경] = useState('');
+  let [생성, set생성] = useState(false);
   return (
-    //JSX 문법
-    //{} 데이터 바인딩
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
@@ -40,11 +36,13 @@ function App() {
           return (
             <div className='list'>
             <h4 onClick={()=>{
-              modal[i] == false ? setModal(true) : setModal(false)
+              setTitle(i)
+              modal == false ? setModal(true) : setModal(false)
               // setModal(true)
             }} >{a}
             </h4>
-            <span onClick={()=>{
+            <span onClick={(e)=>{ 
+              e.stopPropagationtopPr
               let copy = [...따봉넘버];
               copy[i]=copy[i]+1;
               따봉넘버변경(copy);
@@ -53,24 +51,32 @@ function App() {
             </span>{따봉넘버[i]}
             <p>2월 17일 발행</p>
           </div>
-          )        
+          )         
         })
-       }
+       } 
+
+       <input onChange={(e)=>{ 
+        입력값변경(e.target.value)
+        console.log(입력값)
+        ;}}/>
+        <button onClick={()=>{
+          글제목 = [...글제목, 입력값];
+          console.log(글제목);
+          생성 == false ? set생성(true) : set생성(false);
+          }}>생성</button>
        {
-         modal == true ? <Modal modal={modal} color={'skyblue'} 글제목={글제목} 글제목변경={글제목변경}/> : null
+         modal == true ? <Modal title={title} color={'skyblue'} 글제목={글제목} 글제목변경={글제목변경}/> : null
        }
-       <ANT/>
       <h4 style={{ color: 'red', fontSize: '9px' }}>110-572-321917</h4>
       <h4>{post}</h4>
-    </div>
+    </div>    
   );
 }
 
 function Modal(props){
   return (
     <div className='modal' style={{background : props.color}}>
-      <span>{props.modal[0]}</span>
-    <h4>{props.글제목[0]}</h4>
+    <h4>{props.글제목[props.title]}</h4>
     <p>날짜</p>
     <p>상세내용</p>
     <button onClick={()=>{
@@ -82,12 +88,5 @@ function Modal(props){
   )
 }
 
-function ANT(){
-  return(
-    <div className='modal'>
-      <h4>똥</h4>
-    </div>
-  )
-}
 
 export default App;
