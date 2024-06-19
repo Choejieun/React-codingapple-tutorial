944acb0bc3f5deee97a9c390491f724d6c0be4f9
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-06-19 21:35:06 +0900

    리액트 부트스트랩 연습 시작

---
+++ 쇼핑몰 프로젝트/shop/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import { Button, Navbar, Container, Nav, Row, Col  } from 'react-bootstrap';
+import bg from './img/bg.jpg';
+import { useState } from 'react';
+import data from './data.js';
+
+function App() {
+
+  let [shoes] = useState(data)
+
+  return (
+    <div className="App">
+      <Navbar /*</div>bg="dark" data-bs-theme="dark" */className='navBarTop'>
+        <Container>
+          <Navbar.Brand href="#home">아리아리콩</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">홈</Nav.Link>
+            <Nav.Link href="#features">상품</Nav.Link>
+            <Nav.Link href="#pricing">이벤트</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
+      <div>상품 리스트</div>
+      <Container>
+      <Row>
+        <Col xs>
+        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+        <h4>{shoes[0].title}</h4>
+        <p>상품정보</p>
+        </Col>
+        <Col xs={{ order: 12 }}>
+        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{shoes[1].title}</h4>
+            <p>상품정보</p>
+        </Col>
+        <Col xs={{ order: 1 }}>
+        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{shoes[2].title}</h4>
+            <p>상품정보</p>
+          </Col>
+      </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
