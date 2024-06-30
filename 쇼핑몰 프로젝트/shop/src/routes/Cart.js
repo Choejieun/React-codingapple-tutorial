1fe0b8254132adb73ed5c76f0b021e39f1dfedf8
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-06-30 19:44:26 +0900

    리덕스 stat e 변경 학습

--- 쇼핑몰 프로젝트/shop/src/routes/Cart.js
+++ 쇼핑몰 프로젝트/shop/src/routes/Cart.js
@@ -1,35 +1,40 @@
-import {Collapse, Table} from 'react-bootstrap'
-import { useSelector } from 'react-redux'
+import { Collapse, Table } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeName } from '../store'
 
-function Cart(){
+function Cart() {
 
-  let stock = useSelector((state)=> state.stock)
-  console.log(stock[0].id)
+  let stock = useSelector((state) => state.stock)
+  let state = useSelector((state) => state)
+  let dispatch = useDispatch()
 
-    return(
-        <>
-    <Table>
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>상품명</th>
-      <th>수량</th>
-      <th>변경하기</th>
-    </tr>
-  </thead>
-  <tbody>
-    {stock.map((a, i)=>{
-    <tr>
-      <td>{a.id}</td>
-      <td>{a.name}</td>
-      <td>{a.count}</td>
-      <td>{a.id}</td>
-    </tr>
-    })}
-  </tbody>
-</Table> 
-        </>
-    )
+  return (
+    <>
+    {state.user}의 장바구니
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map((a, i) =>
+            <tr key={i}>
+              <td>{a.id}</td>
+              <td>{a.name}</td>
+              <td>{a.count}</td>
+              <td><button onClick={() => {
+                dispatch(changeName())
+              }}>+</button></td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </>
+  )
 }
 
 export default Cart
--- 쇼핑몰 프로젝트/shop/src/store.js
+++ 쇼핑몰 프로젝트/shop/src/store.js
@@ -2,8 +2,16 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 let user = createSlice({
    name : 'user',
-   initialState : 'kim'
+   initialState : 'kim',
+   reducers : {
+      changeName(state){
+        return 'john ' + state
+      }
+   }
 })
+
+export let {changeName } = user.actions
+//오른쪽 자료를 변수로 뺴는 문법일 뿐
 
 let stock = createSlice({
   name : 'stock',
